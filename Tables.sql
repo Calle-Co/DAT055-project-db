e8fb30f34e60d182f6024aba48c4a9df00fefbe2
wilhus <72127741+wilhus@users.noreply.github.com>
2021-03-01 10:33:27 +0100

    LOTS of inserts

--- Tables.sql
+++ Tables.sql
@@ -9,23 +9,17 @@ CREATE TABLE Admin (
     username TEXT PRIMARY KEY UNIQUE,
     password TEXT NOT NULL
 );*/
-
+/*
 DROP TABLE IF EXISTS Seats;
 DROP TABLE IF EXISTS Flights;
 DROP TABLE IF EXISTS Destinations;
-DROP TABLE IF EXISTS Travelers;
 DROP TABLE IF EXISTS Planes;
 
 CREATE TABLE Destinations (
     destination TEXT PRIMARY KEY UNIQUE,
     abbreviation CHAR(3) NOT NULL
 );
-
-CREATE TABLE Travelers (
-    fullname TEXT PRIMARY KEY,
-    age INT NOT NULL
-);
-
+*/
 CREATE TABLE Planes (
     model TEXT PRIMARY KEY,
     seats INT
@@ -45,10 +39,14 @@ CREATE TABLE Flights (
 
 CREATE TABLE Seats (
     flight_id INT,
+    customer TEXT,
     seat CHAR(3),
     isBooked BOOL NOT NULL,
-    PRIMARY KEY (flight_id, seat),
-    FOREIGN KEY (flight_id) REFERENCES Flights(flight_id)
+    traveler TEXT,
+    age INT,
+    PRIMARY KEY (flight_id, customer, seat),
+    FOREIGN KEY (flight_id) REFERENCES Flights(flight_id),
+    FOREIGN KEY (customer) REFERENCES Customers(username)
 );
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO carlcla;
